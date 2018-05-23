cec6a3d6c7fbd058841eb489e3ec6e7a78c632d7
Chaitanya <morechaco@hotmail.com>
2018-05-23 12:23:25 +0530

    Add create db module

---
+++ create_database.py
@@ -0,0 +1,29 @@
+import time
+
+from sqlalchemy import create_engine
+
+from app import app
+
+db_name = app.config.get('DB_NAME')
+user = app.config.get('DB_USER')
+password = app.config.get('DB_PASSWORD')
+host = app.config.get('DB_HOST')
+
+
+def create_db(retry=0):
+    try:
+        mysql_engine = create_engine(
+            'mysql://{}:{}@{}'.format(user, password, host)
+        )
+        result = mysql_engine.execute(
+            'CREATE DATABASE IF NOT EXISTS {}'.format(db_name)
+        )
+        result.close()
+    except Exception:
+        if retry > 3:
+            raise
+        time.sleep(1)
+        create_db(retry + 1)
+
+
+create_db()

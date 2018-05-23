ab483237eac165dce095feed87dd275559ca79ab
Chaitanya <morechaco@hotmail.com>
2018-05-23 14:16:44 +0530

    Register status controller

--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,7 @@ db = SQLAlchemy(app)
 data_store = redis.StrictRedis(
     host=app.config.get('REDIS_HOST'), port=app.config.get('REDIS_PORT')
 )
+
+# Register FlaskView with the app and a base route
+from app.v1.controller.status_controller import StatusController
+StatusController.register(app, route_base='/api/v1')

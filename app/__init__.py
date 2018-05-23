be6d1d02137e8864054b048b1383400b2c5d20a7
Chaitanya <morechaco@hotmail.com>
2018-05-23 12:29:03 +0530

    Add redis configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import redis
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -5,3 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+data_store = redis.StrictRedis(
+    host=app.config.get('REDIS_HOST'), port=app.config.get('REDIS_PORT')
+)

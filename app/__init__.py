40d1bce4496fd2e354f51e9e2256cdd71e5ef886
Chaitanya <morechaco@hotmail.com>
2018-05-24 16:29:03 +0530

    Add sentry configuration

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ import logging
 
 import graypy
 from flask import Flask
+from raven import setup_logging
+from raven.contrib.flask import Sentry
+from raven.handlers.logging import SentryHandler
 
 from app.models import db, data_store
 from app.v1.logging_filters import RequestInfoFilter
@@ -21,7 +24,11 @@ def create_app():
     from app.v1.controller import v1_blueprint
     app.register_blueprint(v1_blueprint, url_prefix='/api/v1')
 
+    # init graylog
     _init_graylog(app)
+
+    # init sentry
+    _init_sentry(app)
 
     return app
 
@@ -34,3 +41,12 @@ def _init_graylog(app):
         gelf_handler.addFilter(RequestInfoFilter())
         app.logger.setLevel(logging.INFO)
         app.logger.addHandler(gelf_handler)
+
+
+def _init_sentry(app):
+    if app.config['ENABLE_SENTRY']:
+        sentry = Sentry(app)
+        sentry_handler = SentryHandler(sentry.client)
+        sentry_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(sentry_handler)
+        setup_logging(sentry_handler)
